1640536a4a14d431e13ffd5f91f9eda6fbce8f84
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-22 11:27:51 -0600

    start task 3.2.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Button from './Button/Button';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import SearchBar from './Searchbar/Searchbar';
+import Modal from './Modal/Modal';
+import { Component } from 'react';
+import { searchPhoto } from 'services/api';
+
+export default class App extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    showModal: false,
+    modalData: null,
+  };
+  // componentDidMount() {
+  //   searchPhoto('cars', 1).then(data => {
+  //     console.log(data);
+  //   });
+  // }
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      console.log('fetch');
+    }
+  }
+
+  getQuery = query => {
+    this.setState({ query, page: 1, images: [] });
+    console.log(query);
+  };
+  handleLoadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+  render() {
+    return (
+      <>
+        <SearchBar getQuery={this.getQuery} />
+        <ImageGallery />
+        <Button handleLoadMore={this.handleLoadMore} />
+        <Loader />
+        {false && <Modal />}
+      </>
+    );
+  }
+}
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,9 @@
+const ImageGalleryItem = () => {
+  return (
+    <li className="ImageGalleryItem">
+      <img className="ImageGalleryItem-image" src="" alt="" />
+    </li>
+  );
+};
+
+export default ImageGalleryItem;
